6673b28b7a8b7d1f99267c03d4fbb9ab38beeee5
antonio-bravo <79697559+antonio-bravo@users.noreply.github.com>
2026-07-17 10:34:52 +0200

    fix workflow

--- app/src/main/java/com/tonio/libre2clock/data/model/LibreModels.kt
+++ app/src/main/java/com/tonio/libre2clock/data/model/LibreModels.kt
@@ -6,71 +6,71 @@ import kotlinx.serialization.Serializable
 
 @JsonClass(generateAdapter = true)
 data class LoginRequest(
-    @Json(name = "email") val email: String,
-    @Json(name = "password") val password: String
+    @param:Json(name = "email") val email: String,
+    @param:Json(name = "password") val password: String
 )
 
 @JsonClass(generateAdapter = true)
 data class LoginResponse(
-    @Json(name = "status") val status: Int,
-    @Json(name = "data") val data: LoginData?
+    @param:Json(name = "status") val status: Int,
+    @param:Json(name = "data") val data: LoginData?
 )
 
 @JsonClass(generateAdapter = true)
 data class LoginData(
-    @Json(name = "user") val user: User,
-    @Json(name = "authTicket") val authTicket: AuthTicket
+    @param:Json(name = "user") val user: User,
+    @param:Json(name = "authTicket") val authTicket: AuthTicket
 )
 
 @JsonClass(generateAdapter = true)
 data class User(
-    @Json(name = "id") val id: String,
-    @Json(name = "email") val email: String
+    @param:Json(name = "id") val id: String,
+    @param:Json(name = "email") val email: String
 )
 
 @JsonClass(generateAdapter = true)
 data class AuthTicket(
-    @Json(name = "token") val token: String,
-    @Json(name = "expires") val expires: Long
+    @param:Json(name = "token") val token: String,
+    @param:Json(name = "expires") val expires: Long
 )
 
 @JsonClass(generateAdapter = true)
 data class ConnectionsResponse(
-    @Json(name = "status") val status: Int,
-    @Json(name = "data") val data: List<Connection>?
+    @param:Json(name = "status") val status: Int,
+    @param:Json(name = "data") val data: List<Connection>?
 )
 
 @JsonClass(generateAdapter = true)
 data class Connection(
-    @Json(name = "patientId") val patientId: String,
-    @Json(name = "firstName") val firstName: String,
-    @Json(name = "lastName") val lastName: String,
-    @Json(name = "glucoseMeasurement") val glucoseMeasurement: GlucoseMeasurement?
+    @param:Json(name = "patientId") val patientId: String,
+    @param:Json(name = "firstName") val firstName: String,
+    @param:Json(name = "lastName") val lastName: String,
+    @param:Json(name = "glucoseMeasurement") val glucoseMeasurement: GlucoseMeasurement?
 )
 
 @JsonClass(generateAdapter = true)
 data class GlucoseResponse(
-    @Json(name = "status") val status: Int,
-    @Json(name = "data") val data: GlucoseData?
+    @param:Json(name = "status") val status: Int,
+    @param:Json(name = "data") val data: GlucoseData?
 )
 
 @JsonClass(generateAdapter = true)
 data class GlucoseData(
-    @Json(name = "connection") val connection: Connection,
-    @Json(name = "activeSensors") val activeSensors: List<ActiveSensor>?,
-    @Json(name = "graphData") val graphData: List<GlucoseMeasurement>
+    @param:Json(name = "connection") val connection: Connection,
+    @param:Json(name = "activeSensors") val activeSensors: List<ActiveSensor>?,
+    @param:Json(name = "graphData") val graphData: List<GlucoseMeasurement>
 )
 
 @JsonClass(generateAdapter = true)
 data class ActiveSensor(
-    @Json(name = "sensor") val sensor: Sensor
+    @param:Json(name = "sensor") val sensor: Sensor
 )
 
 @JsonClass(generateAdapter = true)
 data class Sensor(
-    @Json(name = "sn") val serialNumber: String,
-    @Json(name = "a") val activationTimestamp: Long, // Unix epoch seconds
-    @Json(name = "w") val warmupMinutes: Int
+    @param:Json(name = "sn") val serialNumber: String,
+    @param:Json(name = "a") val activationTimestamp: Long, // Unix epoch seconds
+    @param:Json(name = "w") val warmupMinutes: Int
 )
 
 data class SensorStatus(
@@ -82,12 +82,12 @@ data class SensorStatus(
 @JsonClass(generateAdapter = true)
 @Serializable
 data class GlucoseMeasurement(
-    @Json(name = "FactoryTimestamp") val factoryTimestamp: String,
-    @Json(name = "Timestamp") val timestamp: String,
-    @Json(name = "type") val type: Int,
-    @Json(name = "ValueInMgPerDl") val valueInMgPerDl: Int,
-    @Json(name = "TrendArrow") val trendArrow: Int?,
-    @Json(name = "MeasurementColor") val measurementColor: Int?,
-    @Json(name = "Value") val value: Int,
+    @param:Json(name = "FactoryTimestamp") val factoryTimestamp: String,
+    @param:Json(name = "Timestamp") val timestamp: String,
+    @param:Json(name = "type") val type: Int,
+    @param:Json(name = "ValueInMgPerDl") val valueInMgPerDl: Int,
+    @param:Json(name = "TrendArrow") val trendArrow: Int?,
+    @param:Json(name = "MeasurementColor") val measurementColor: Int?,
+    @param:Json(name = "Value") val value: Int,
     val calibratedValue: Int = value
 )
